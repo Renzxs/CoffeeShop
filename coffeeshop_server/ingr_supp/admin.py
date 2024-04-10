0cb94a5ba332b4e5aab2afa83fed527ba2b8d97f
CenRIZZa <cenizamaki@gmail.com>
2024-04-10 13:54:08 +0800

    Database for ingr_supp

--- coffeeshop_server/ingr_supp/admin.py
+++ coffeeshop_server/ingr_supp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Ingr,Supp
 
-# Register your models here.
+@admin.register(Ingr)
+class IngrAdmin(admin.ModelAdmin):
+    list_display = ('ingr_name', 'ingr_quant', 'ingr_desc', 'ingr_catg', 'ingr_cost', 'ingr_suppNo', 'ingr_exp', 'ingr_batch', 'ingr_date', 'ingr_act')
+
+@admin.register(Supp)
+class SuppAdmin(admin.ModelAdmin):
+    list_display = ('Supp_name', 'Supp_quant', 'Supp_desc', 'Supp_catg', 'Supp_cost', 'Supp_suppNo', 'Supp_exp', 'Supp_batch', 'Supp_date', 'Supp_act')
--- coffeeshop_server/ingr_supp/models.py
+++ coffeeshop_server/ingr_supp/models.py
@@ -3,4 +3,43 @@ from django.db import models
 # Create your models here.
 class Ingr(models.Model):
     ingr_name = models.CharField(max_length= 50)
-    ingr_quant = models.IntegerField()
+    ingr_quant = models.IntegerField(default=0)
+    ingr_desc = models.CharField(max_length=50)
+    ingr_catg = models.CharField (max_length=50)
+    ingr_cost = models.FloatField(default=0.0)
+    ingr_suppNo = models.CharField(max_length=50)
+    ingr_exp = models.DateField()
+    ingr_batch = models.CharField(max_length=5, default='00001')
+    ingr_date = models.DateField()
+    ingr_act = models.BooleanField(default=True)
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            # This is a new object, increment the batch_number
+            last_object = Ingr.objects.last()
+            if last_object:
+                # Increment the last batch_number
+                next_batch_number = str(int(last_object.ingr_batch) + 1).zfill(5)
+                self.ingr_batch = next_batch_number  # Corrected line
+        super().save(*args, **kwargs)
+
+class Supp(models.Model):
+    Supp_name = models.CharField(max_length= 50)
+    Supp_quant = models.IntegerField(default=0)
+    Supp_desc = models.CharField(max_length=50)
+    Supp_catg = models.CharField (max_length=50)
+    Supp_cost = models.FloatField(default=0.0)
+    Supp_suppNo = models.CharField(max_length=50)
+    Supp_exp = models.DateField()
+    Supp_batch = models.CharField(max_length=5, default='00001')
+    Supp_date = models.DateField()
+    Supp_act = models.BooleanField(default=True)
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            # This is a new object, increment the batch_number
+            last_object = Supp.objects.last()
+            if last_object:
+                # Increment the last batch_number
+                next_batch_number = str(int(last_object.Supp_batch) + 1).zfill(5)
+                self.Supp_batch = next_batch_number  # Corrected line
+        super().save(*args, **kwargs)
+
