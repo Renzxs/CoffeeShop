b38ac52262be12dbdc5a3bcfb561b383a905350a
CenRIZZa <cenizamaki@gmail.com>
2024-04-11 02:31:09 +0800

    CRU for ingr_supp

--- coffeeshop_server/ingr_supp/urls.py
+++ coffeeshop_server/ingr_supp/urls.py
@@ -7,8 +7,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('stocks',views.view, name="view"),
-     path('ingredients', views.show_ingredients, name="ingredients"),
+    path('ingr',views.ingr, name = "ingr"),
     path('products',views.prod, name="prod"),
+    path('add_ingredient/', views.add_ingredient, name='add_ingredient'),
+    path('update_ingredient/<int:ingredient_id>/', views.update_ingredient, name='update_ingredient'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- coffeeshop_server/ingr_supp/views.py
+++ coffeeshop_server/ingr_supp/views.py
@@ -1,19 +1,56 @@
-from django.shortcuts import render
-
-from ingr_supp.models import Ingr,Supp
+import json
+from django.http import HttpResponseServerError
+from django.shortcuts import get_object_or_404, render
+from django.views.decorators.csrf import csrf_exempt
+from .models import Ingr,Supp
 
 # Create your views here.
 def view(request):
     supplies = Supp.objects.all()
     return render(request,'home.html',{'supplies':supplies})
 
-def ingr(request):
-    return render(request,'ingr.html')
-
 def prod(request):
     return render(request,'prod.html')
 
-def show_ingredients(request):
+def ingr(request):
     ingredients = Ingr.objects.all()
-    return render(request, 'ingr.html', {'ingredients': ingredients})
+    return render(request, 'ingredients.html', {'ingredients':ingredients})
 
+
+from django.shortcuts import render, redirect
+from .models import Ingr
+from .forms import IngrForm, IngrFormUp
+
+def add_ingredient(request):
+    if request.method == 'POST':
+        form = IngrForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('ingr')  # Redirect to the ingredients page after adding
+    else:
+        form = IngrForm()
+    return render(request, 'ingredients.html', {'form': form})
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponseServerError
+from .forms import IngrFormUp
+from .models import Ingr
+
+def update_ingredient(request, ingredient_id):
+    try:
+        ingredient = Ingr.objects.get(pk=ingredient_id)
+    except Ingr.DoesNotExist:
+        return HttpResponseServerError("Ingredient not found")
+
+    if request.method == 'POST':
+        form = IngrFormUp(request.POST, instance=ingredient)
+        if form.is_valid():
+            form.save()
+            return redirect('ingr')  # Redirect to the ingredients page after updating
+        else:
+            # Redirect to error.html if form is invalid
+            return render(request, 'error.html', {'form': form})
+    else:
+        form = IngrFormUp(instance=ingredient)
+    
+    return render(request, 'ingredients.html', {'form': form, 'ingredient_id': ingredient_id})
